0fa263b1cf30c36dd1e8726c4c803e4e377f2724
angad-sr <30727066+angad-sr@users.noreply.github.com>
2018-04-10 17:29:26 +0800

    d3 core change

--- Scripts/viz-Clustering.js
+++ Scripts/viz-Clustering.js
@@ -2704,37 +2704,7 @@
     }
     return [ a, c ];
   };
-  d3.random = {
-    normal: function(µ, s) {
-      var n = arguments.length;
-      if (n < 2) s = 1;
-      if (n < 1) µ = 0;
-      return function() {
-        var x, y, r;
-        do {
-          x = Math.random() * 2 - 1;
-          y = Math.random() * 2 - 1;
-          r = x * x + y * y;
-        } while (!r || r > 1);
-        return µ + s * x * Math.sqrt(-2 * Math.log(r) / r);
-      };
-    },
-    logNormal: function(µ, s) {
-      var n = arguments.length;
-      if (n < 2) s = 1;
-      if (n < 1) µ = 0;
-      var random = d3.random.normal();
-      return function() {
-        return Math.exp(µ + s * random());
-      };
-    },
-    irwinHall: function(m) {
-      return function() {
-        for (var s = 0, j = 0; j < m; j++) s += Math.random();
-        return s / m;
-      };
-    }
-  };
+
   d3.sum = function(array, f) {
     var s = 0, n = array.length, a, i = -1;
     if (arguments.length === 1) {
